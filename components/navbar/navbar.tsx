39acfb33b438ae8f0753dd17ce3c81876c7f40cf
vanderms <vanderms.84@gmail.com>
2022-06-01 12:17:39 -0300

    implements navbar mobile design

---
+++ components/navbar/navbar.tsx
@@ -0,0 +1,43 @@
+import Link from "next/link";
+import Image from "next/image";
+import { useState } from "react";
+
+
+export const Navbar: React.FC = () => {
+  const [sidebar, setSidebar] = useState<{ open: boolean }>({ open: false });
+  const closeSidebar = () => setSidebar({ open: false });
+  const openSidebar = () => setSidebar({ open: true });
+
+  
+
+  return (
+    <nav className="nv__navbar">
+      <Link href='/'>
+        <a className="logo">
+          <div className="wrapper-image">
+            <Image width={76} height={76} src='/assets/logo.svg'></Image>
+          </div>
+          <span>Excursy</span>
+        </a>
+      </Link>
+      <div className="sidebar-mobile" data-open={sidebar.open}>
+        <button className="close-btn" aria-label="close sidebar menu" onClick={closeSidebar}>
+          <Image src='/assets/icon/close.svg' width={20} height={20}></Image>
+        </button>       
+        <ul className="navlink-list">
+          <li><Link href='/'><a className="navlink">Home</a></Link></li>
+          <li><a className="navlink" href="#about-us">About us</a></li>
+          <li><a className="navlink" href="#our-interests">Our interests</a></li>
+          <li><a className="navlink" href="#values">Values</a></li>
+          <li><a className="navlink" href="#current-projects">Current projects</a></li>
+        </ul>
+        <Link href='/'><a className="call-to-action">Sign Up</a></Link>
+      </div>
+      <div className="backdrop" data-open={sidebar.open} onClick={closeSidebar}></div>
+      <button className="btn-menu" aria-label="menu" onClick={openSidebar}>
+        <Image src='/assets/icon/hamburger.svg' width={29} height={24}></Image>
+      </button>
+    </nav>
+  )
+}
+
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import { Navbar } from '../components/navbar/navbar';
 
 const Home: NextPage = () => {
   return (
@@ -8,7 +9,7 @@ const Home: NextPage = () => {
         <title>Excursy - Blockchain Tech</title>       
         <link rel="icon" type="image/png" href="./ico.png" />
       </Head>
-
+      <Navbar/>
       
     </>
   )
