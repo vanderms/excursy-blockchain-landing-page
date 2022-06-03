bec0b4f775707291d75e2b7c1bec4ff86bb943a4
vanderms <vanderms.84@gmail.com>
2022-06-03 20:37:57 -0300

    add og meta tags to index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,34 @@ import { Footer } from '../components/sections/footer/footer';
 const Home: NextPage = () => {
   return (
     <>
-      <Head>
+      <Head>     
+        {/* <!-- Site Name, Title, and Description to be displayed --> */}
+        <meta
+          property="og:site_name"
+          content="Excursy - Blockchain tech"
+        />
+        <meta property="og:title" content="Pod Request Landing Page" />
+        <meta
+          property="og:description"
+          content="Excursy is a landing page designed for blockchain tech companies"
+        />
+        {/* <!-- Image to display -->  */}
+        {/* <!-- Replace   «example.com/image01.jpg» with your own -->  */}
+        <meta
+          property="og:image"
+          content="https://precious-platypus-443963.netlify.app/preview.jpg"
+        />
+
+        {/* <!-- No need to change anything here -->  */}
+        <meta property="og:type" content="website" />
+        <meta property="og:image:type" content="image/jpeg" />
+
+        {/* <!-- Website to visit when clicked social medias-->  */}
+        <meta
+          property="og:url"
+          content="https://precious-platypus-443963.netlify.app/"
+        ></meta>
+        
         <title>Excursy - Blockchain Tech</title>       
         
       </Head>
